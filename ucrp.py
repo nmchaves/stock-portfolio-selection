3051b7a7d489f6dce7ba50e943fe29ca02d15205
nicochaves <nico.chaves@yahoo.com>
2016-05-30 14:50:57 -0700

    Updated UCRP.

--- ucrp.py
+++ ucrp.py
@@ -8,27 +8,25 @@ class UniformConstantRebalancedPortfolio(Portfolio):
 
         UCRP serves as a very simple baseline.
     """
+    def __init__(self, market_data, start=0, stop=None, rebal_interval=1, tune_interval=None,
+                 init_b=None, verbose=False, silent=False, past_results_dir=None,
+                 new_results_dir=None, repeat_past=False):
+        self.portfolio_type = 'UCRP'
 
-    def run(self):
-        print 'Running UCRP'
-        for day in range(0, self.num_days):
-            self.update(day)
-        self.print_results()
-        self.save_results()
+        super(UniformConstantRebalancedPortfolio, self).__init__(
+                                market_data=market_data, start=start, stop=stop, rebal_interval=rebal_interval,
+                                init_b=init_b, tune_interval=tune_interval, verbose=verbose, silent=silent,
+                                past_results_dir=past_results_dir, new_results_dir=new_results_dir, repeat_past=repeat_past)
 
-    def get_new_allocation(self, cur_day):
+    def get_new_allocation(self, cur_day, init=False):
         cur_day_op = self.data.get_op(relative=False)[cur_day, :]  # opening prices on |cur_day|
         new_b = util.get_uniform_allocation(self.num_stocks, cur_day_op)
         return new_b
 
     def print_results(self):
-        print 30 * '-'
-        print 'Performance for uniform constant rebalancing portfolio:'
-        print 30 * '-'
-        Portfolio.print_results(self)
-        print self.b_history[-1]
-
-    def save_results(self):
-        output_fname = 'results/new/const_rebalancing_dollars_over_time.txt'
-        util.save_results(output_fname, self.dollars_history)
+        if self.verbose:
+            print 30 * '-'
+            print 'Performance for uniform constant rebalancing portfolio:'
+            print 30 * '-'
+            Portfolio.print_results(self)
 
